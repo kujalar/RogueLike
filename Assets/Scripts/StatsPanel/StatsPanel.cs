66d36506b54892324cdcd99fed00df1e8a5d5870
Ari Kujala <ari.kujala@kotiposti.net>
2017-12-26 23:27:53 +0200

    started with stats panel

---
+++ Assets/Scripts/StatsPanel/StatsPanel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatsPanel : MonoBehaviour {
+	public Text moveText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Player selectedPlayer = GameManager.instance.getSelectedPlayer (); 
+		if (selectedPlayer == null) {
+			return;
+		}
+		if (selectedPlayer.movePointsLeftDirty) {
+			selectedPlayer.movePointsLeftDirty = false;
+			Speed speed = selectedPlayer.getMovePointsLeft ();
+			moveText.text = "Move "+speed.currentValue+"("+speed.maxValue+")";
+		}
+	}
+}
--- Assets/Scripts/controlpanel/ControlPanel.cs
+++ Assets/Scripts/controlpanel/ControlPanel.cs
@@ -4,22 +4,12 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class ControlPanel : MonoBehaviour {
-	public Text movePoints;
 
 	// Use this for initialization
 	void Start () {
 		
 	}
 	void Update(){
-		Player selectedPlayer = GameManager.instance.getSelectedPlayer (); 
-		if (selectedPlayer == null) {
-			return;
-		}
-		if (selectedPlayer.movePointsLeftDirty) {
-			selectedPlayer.movePointsLeftDirty = false;
-			Speed speed = selectedPlayer.getMovePointsLeft ();
-			Debug.Log ("Setting mp left="+speed.currentValue);
-			movePoints.text = ""+speed.currentValue;
-		}
+		
 	}
 }
