d0a879cd7b9bc646d2b439c651e958c1e0f8d409
Ari Kujala <ari.kujala@kotiposti.net>
2017-12-28 01:06:46 +0200

    actionHolders in statPanel are now dynamic, version 1

---
+++ Assets/Scripts/StatsPanel/ActionHolder.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ActionHolder : MonoBehaviour {
+	public Text actionText;
+	// Use this for initialization
+	void Awaken () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void setText(string text){
+		actionText.text = text;
+	}
+}
--- Assets/Scripts/StatsPanel/StatsPanel.cs
+++ Assets/Scripts/StatsPanel/StatsPanel.cs
@@ -4,16 +4,68 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class StatsPanel : MonoBehaviour {
-	public Text moveText;
+	private Text moveText;
+	public GameObject moveTextObject;
+	public GameObject fab_ActionHolder;
 
+	private float firstActionRow = -22f;
+	private float actionRowHeight = -14f;
+	private GameObject actionHolder = null;
+	private GameObject bonusActionHolder = null;
+	private GameObject reactionHolder = null;
+
+	private List<GameObject> panelRows = new List<GameObject>();
+	private bool componentPositioningDirty=false;
 	// Use this for initialization
 	void Start () {
-		
+		/*actionHolder = Instantiate (fab_ActionHolder) as GameObject; //, new Vector3(100f,100f, 0f), Quaternion.identity) as GameObject;
+		instance.transform.SetParent(gameObject.transform);
+		instance.transform.localPosition = new Vector3 (6f, firstActionRow, 0f);*/
+		actionHolder = initActionHolder (new Color(12,238,33));
+		bonusActionHolder = initActionHolder (new Color(84,7,244));
+		reactionHolder = initActionHolder (new Color (0,0,0));
+
+		moveText = moveTextObject.GetComponent<Text> ();
+		panelRows.Add (moveTextObject);
+
+		Debug.Log ("Start StatsPanel");
+		componentPositioningDirty = true;
+	}
+	private GameObject initActionHolder(Color color){
+		GameObject instance = Instantiate (fab_ActionHolder) as GameObject;
+		instance.transform.SetParent (gameObject.transform);
+		instance.transform.localPosition = new Vector3 (6f, firstActionRow, 0f);
+		panelRows.Add (instance);
+		return instance;
 	}
 	
+
+
+	void RemoveActions(){
+		actionHolder.SetActive (false);
+		bonusActionHolder.SetActive (false);
+		reactionHolder.SetActive (false);
+	}
+	void AddAction(){
+		//action holder is always the first row
+		actionHolder.SetActive (true);
+	}
+	void AddBonusAction(){
+		bonusActionHolder.SetActive (true);
+	}
+	void AddReaction(){
+		reactionHolder.SetActive (true);
+	}
+
 	// Update is called once per frame
 	void Update () {
 		Player selectedPlayer = GameManager.instance.getSelectedPlayer (); 
+
+		UpdateMovePointsLeft (selectedPlayer);
+		UpdateComponentPositioning ();
+	}
+
+	private void UpdateMovePointsLeft(Player selectedPlayer){
 		if (selectedPlayer == null) {
 			return;
 		}
@@ -21,6 +73,25 @@ public class StatsPanel : MonoBehaviour {
 			selectedPlayer.movePointsLeftDirty = false;
 			Speed speed = selectedPlayer.getMovePointsLeft ();
 			moveText.text = "Move "+speed.currentValue+"("+speed.maxValue+")";
+		}
+	}
+
+	private void UpdateComponentPositioning(){
+		if (!componentPositioningDirty) {
+			return;
+		}
+		Debug.Log ("UpdateComponentPositioning");
+		componentPositioningDirty = false;
+
+		float count = 0f;
+		for (int i = 0; i < panelRows.Count; i++) {
+			GameObject holder = panelRows [i];
+			if (holder.activeSelf) {
+				float y = firstActionRow + (count * actionRowHeight);
+				Debug.Log ("y="+y);
+				holder.transform.localPosition= new Vector3 (6f,y,0f);
+				count += 1f;
+			}
 		}
 	}
 }
