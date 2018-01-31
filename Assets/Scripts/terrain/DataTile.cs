903679e74ba2e93aa4bc1b810338570c092b2783
Ari Kujala <ari.kujala@kotiposti.net>
2018-01-31 23:01:43 +0200

    Changed moverule to datatile and databrush and made first prototype of them

---
+++ Assets/Scripts/terrain/DataTile.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEditor;
+
+public class DataTile : Tile
+{
+    [SerializeField]
+    private Sprite[] dataSprites;
+    [SerializeField]
+    private Sprite preview;
+    //wall data tells what wall code we have on each side of the tile. it is used to check if the tile can be entered.
+    public string wallData = "        ";
+    //this function is called every time we paint a tile into tilemap.
+    public override void RefreshTile(Vector3Int position, ITilemap tilemap)
+    {
+        base.RefreshTile(position, tilemap);
+    }
+    /// <summary>
+    /// Changes the tiles sprite to the correct sprites based on the situation
+    /// </summary>
+    /// <param name="location">The location of this sprite</param>
+    /// <param name="tilemap">A reference to the tilemap, that this tile belongs to</param>
+    /// <param name="tileData">A reference to the actual object, that this tile belongs to</param>
+    public override void GetTileData(Vector3Int location, ITilemap tilemap, ref TileData tileData)
+    {
+        //base.GetTileData(location, tilemap,ref tileData);
+        tileData.sprite = preview;
+    }
+
+    public void setData(string data)
+    {
+        wallData = data;
+    }
+    public string getData()
+    {
+        return wallData;
+    }
+    
+
+#if UNITY_EDITOR
+    //This will place a new object into Unity Editors menu. Check Assets -> Create -> Tiles -> DataTile
+    [MenuItem("Assets/Create/Tiles/DataTile")]
+    public static void CreateDataTile()
+    {
+        //This will execute when someone selects the WaterTile menu item.
+        string path = EditorUtility.SaveFilePanelInProject("Save DataTile", "NewDataTile", "asset", "Saved DataTile", "Assets");
+        if (path == "")
+        {
+            return;
+        }
+        AssetDatabase.CreateAsset(ScriptableObject.CreateInstance<DataTile>(), path);
+
+    }
+#endif
+
+}
---
+++ Assets/tilemap/brushes/DataBrush.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomGridBrush(true, false, false, "Data Brush")]
+public class DataBrush : GridBrush
+{
+
+    public GameObject targetTilemap;
+    public string wallCode;
+
+    public override void Paint(GridLayout gridLayout, GameObject brushTarget, Vector3Int position)
+    {
+        //we will write only to the dataTileMap
+        // this.SetTile(new Vector3Int(0, 0, 0), new DataTile());
+        DataTile dataTile = cells[0].tile as DataTile;
+        if (dataTile == null)
+        {
+            Debug.LogWarning("Please use DataTile while painting with Data brush! Now you did not.");
+            return;
+        }
+        dataTile.setData(wallCode);
+        base.Paint(gridLayout, brushTarget, position);
+        Debug.Log("CellCount=" + cellCount + " We would paint " + brushTarget.name + " with " + wallCode + " if we would gridLayout=" + gridLayout.name + " brushTarget=" + brushTarget.name);
+    }
+#if UNITY_EDITOR
+        [MenuItem("Assets/Create/Brushes/Data Brush")]
+        public static void CreateBrush()
+        {
+            string path = EditorUtility.SaveFilePanelInProject("Save Data Brush", "NewDataBrush", "Asset", "Save Data Brush", "Assets");
+            if (path == "")
+                return;
+            AssetDatabase.CreateAsset(ScriptableObject.CreateInstance<DataBrush>(), path);
+        }
+#endif
+    }
+
+    [CustomEditor(typeof(DataBrush))]
+    public class MoveRuleBrushEditor : GridBrushEditor
+    {
+        public override void OnPaintInspectorGUI()
+        {
+            base.OnPaintInspectorGUI();
+        }
+        public override void OnPaintSceneGUI(GridLayout gridLayout, GameObject brushTarget, BoundsInt position, GridBrushBase.Tool tool, bool executing)
+        {
+            base.OnPaintSceneGUI(gridLayout, brushTarget, position, tool, executing);
+            //this is when our editor is painting the scene
+        }
+    }
+
